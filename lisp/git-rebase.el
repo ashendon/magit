ashendon/magit

commit 12f38035900fb7fb721481898281a091bbb1f29f

git-rebase-line regexp did not support spaces before pick commands
* This causes it to reject lines like "# pick ..." generated by
  magit-rebase when using git-rebase-kill-line

lisp/git-rebase.el
@@ -467,7 +467,7 @@ running 'man git-rebase' at the command line) for details."
   (setq comment-start (or (magit-get "core.commentChar") "#"))
   (setq git-rebase-comment-re (concat "^" (regexp-quote comment-start)))
   (setq git-rebase-line
-        (concat "^\\(" (regexp-quote comment-start) "?"
+        (concat "^\\(" (regexp-quote comment-start) "? *"
                 "\\(?:[fprse]\\|pick\\|reword\\|edit\\|squash\\|fixup\\|exec\\|noop\\)\\) "
                 "\\(?:\\([^ \n]+\\) \\(.*\\)\\)?"))
   (setq font-lock-defaults (list (git-rebase-mode-font-lock-keywords) t t))

commit 7a41340d289b114f7e593c668226bae3e8e2d9f1

git-rebase-move-line-down: Fix max bound definition
We narrow to the set of rebase lines when moving a line down because,
if git-rebase-show-instructions is non-nil, we don't want to move a
line down into the comments.  But this procedure has been broken since
12f38035 (git-rebase-line regexp did not support spaces before pick
commands, 2017-06-25).  With the new git-rebase-line regexp, the
search matches a line in the commented instructions block, setting the
maximum bound too high.

A further complication is that, once we add support for
--rebase-merges, there will be blank lines and "# Branch <label>"
comments among the rebase lines, so we can't just stop the first time
git-rebase-line-p is nil.

Another option would be to set the max bound based on a search for the
first line of the instructions block, but this isn't a good approach
because the "# Rebase ..." line is marked for translation.

lisp/git-rebase.el
@@ -285,10 +285,19 @@ current line."
     (save-restriction
       (narrow-to-region
        (point-min)
-       (1+ (save-excursion
-             (goto-char (point-min))
-             (while (re-search-forward git-rebase-line nil t))
-             (point))))
+       (1-
+        (if git-rebase-show-instructions
+            (save-excursion
+              (goto-char (point-min))
+              (while (or (git-rebase-line-p)
+                         ;; The output for --rebase-merges has empty
+                         ;; lines and "Branch" comments interspersed.
+                         (looking-at-p "^$")
+                         (looking-at-p (concat git-rebase-comment-re
+                                               " Branch")))
+                (forward-line))
+              (line-beginning-position))
+          (point-max))))
       (if (or (and (< n 0) (= beg (point-min)))
               (and (> n 0) (= end (point-max)))
               (> end (point-max)))